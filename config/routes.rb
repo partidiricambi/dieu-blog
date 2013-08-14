0e881cd06aeef0b7982466aa933841d129487a9e
D <kawayida@gmail.com>
2013-08-14 22:42:13 +0100

    Update routes.rb for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -34,14 +34,7 @@ Blog::Application.routes.draw do
   #   end
   #index
    
-  SampleApp::Application.routes.draw do
-    match '/help',    to: 'static_pages#help',    via: 'get'
-    match '/about',   to: 'static_pages#about',   via: 'get'
-    match '/contact', to: 'static_pages#contact', via: 'get'
-    .
-    .
-    .
-  end
+  
   
   # Example resource route with sub-resources:
   #   resources :products do
